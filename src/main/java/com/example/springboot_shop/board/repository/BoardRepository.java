9a8537fe2b81e5c7feb1e69a2a649950311cd628
Jeon Jaehyeong <wjswogud789@gmail.com>
2023-03-03 17:30:35 +0900

    fdf

--- src/main/java/com/example/springboot_shop/board/repository/BoardRepository.java
+++ src/main/java/com/example/springboot_shop/board/repository/BoardRepository.java
@@ -2,12 +2,11 @@ package com.example.springboot_shop.board.repository;
 
 
 import com.example.springboot_shop.board.entity.Board;
-import com.example.springboot_shop.board.repository.query.BoardQueryRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<Board, Long>, BoardQueryRepository {
+public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Page<Board> findAllByMemberId(Long memberId, Pageable pageable);
     
